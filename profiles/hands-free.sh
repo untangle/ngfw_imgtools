503e27789ba6c60ee725d14efccf55c188cf6e37
Chris Blaise <cblaise@untangle.com>
2021-03-19 15:06:07 -0400

    NGFW-13624 Look for and ignore the installer device entirely.

--- profiles/hands-free.sh
+++ profiles/hands-free.sh
@@ -19,16 +19,25 @@ fi
 allow_hands_free=0
 
 total_parts=0
-
 has_ngfw_root=0
 vfat_parts=0
 swap_parts=0
 
-blk_devices=$(blkid | sed -n -e 's/\([^:]*\):.* TYPE="\([^"]*\)".*/\1 \2/p')
-mkdir -p $mount_test
+## Ignore all partitions from the installer media.
+rootname=$(dirname $script | sed -n -e 's#^\(/[^/]*\).*#\1#p')
+installer_dev=$(mount | grep $rootname | cut -d' ' -f1)
+echo $installer_dev >> /tmp/dbg.txt
+if [ "$installer_dev" = "" ] ; then
+    installer_dev=ignoreme
+fi
+
+blk_devices=$(blkid | grep -v "^$installer_dev:" | sed -n -e 's/\([^:]*\):.* TYPE="\([^"]*\)".*/\1 \2/p')
 while read line; do
    part=${line%% *}
    type=${line##* }
+   if [ "$part" = "" ]; then
+       continue
+   fi
    total_parts=`expr $total_parts + 1`
    if [ "$type" = "ext4" ] ; then
       blkid | grep $part: | grep -q 'LABEL="NGFW_ROOT"'
@@ -49,14 +58,14 @@ done << EOT
 $(echo -e "$blk_devices")
 EOT
 
-if [ $total_parts -eq 1 ] ; then
+if [ $total_parts -eq 0 ] ; then
     # Only see one partition which is us - enable hands free.
     allow_hands_free=1
 fi
 
 if [ $has_ngfw_root -eq 1 \
     -a $swap_parts -eq 1 \
-    -a $total_parts -eq 3 ] ; then
+    -a $total_parts -eq 2 ] ; then
     # Found ngfw root, swap, and installer partitions - enable hands free
     allow_hands_free=1
 fi
@@ -64,7 +73,7 @@ fi
 if [ $has_ngfw_root -eq 1 \
     -a $swap_parts -eq 1 \
     -a $vfat_parts -eq 1 \
-    -a $total_parts -eq 4 ] ; then
+    -a $total_parts -eq 3 ] ; then
     # Found ngfw root, efi (vfat), swap, and installer partitions - enable hands free
     allow_hands_free=1
 fi
